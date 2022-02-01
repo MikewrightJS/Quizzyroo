21df90d8c88d5e28959c30673713d80181d2b654
mcmikey123 <77508686+mcmikey123@users.noreply.github.com>
2022-02-01 11:45:36 +0000

    remove child process

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-(function() {
-  var childProcess = require("child_process");
-  var oldSpawn = childProcess.spawn;
-  function mySpawn() {
-      console.log('spawn called');
-      console.log(arguments);
-      var result = oldSpawn.apply(this, arguments);
-      return result;
-  }
-  childProcess.spawn = mySpawn;
-})();
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
